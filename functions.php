d368bf30323442b300ceb5bcd5857323349de1ee
cody <hi@codywhitby.net>
2021-01-14 09:45:15 -0500

    rm opensans admin

--- functions.php
+++ functions.php
@@ -192,6 +192,16 @@ function jbc_scripts()
 }
 add_action('wp_enqueue_scripts', 'jbc_scripts');
 
+// Remove Open Sans that WP adds from frontend
+if (!function_exists('remove_wp_open_sans')) :
+	function remove_wp_open_sans()
+	{
+		wp_deregister_style('open-sans');
+		wp_register_style('open-sans', false);
+	}
+	add_action('wp_enqueue_scripts', 'remove_wp_open_sans');
+endif;
+
 /**
  * Implement the Custom Header feature.
  */
