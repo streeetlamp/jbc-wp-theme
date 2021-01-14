0561a8869e7e8ecceb6e3ba4cf84b062dd5171bc
cody <hi@codywhitby.net>
2021-01-14 13:49:37 -0500

    init home quote

--- page.php
+++ page.php
@@ -26,50 +26,37 @@ get_header();
 			endwhile;
 		endif;
 	endif;
-	wp_reset_postdata(); ?>
+	wp_reset_postdata();
+	?>
 
+	<nav id="alt-nav" class="alternative-navigation">
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'alt-nav',
+				'menu_id'        => 'Alternative',
+			)
+		);
+		wp_reset_postdata(); ?>
+	</nav><!-- #alt-navigation -->
 
 	<?php
-
 	if (is_front_page()) : ?>
 		<?php
-		$the_query = new WP_Query(array('post_type' => 'quotes'));
+		$arg = array(
+			'orderby'        => 'rand',
+			'post_type'			=> 'quotes',
+			"numberposts" => 1,
+			"posts_per_page" => 1,
+		);
+		$the_query = new WP_Query($arg);
 		if ($the_query->have_posts()) :
 			while ($the_query->have_posts()) : $the_query->the_post(); ?>
-				<h2><?php the_content(); ?></h2>
-		<?php endwhile;
+				<div class="home-quote"><?php the_content() ?></div>
+	<?php endwhile;
 		endif;
-		wp_reset_postdata(); ?>
-
-		<nav id="alt-nav" class="alternative-navigation">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'alt-nav',
-					'menu_id'        => 'Alternative',
-				)
-			);
-			?>
-		</nav><!-- #alt-navigation -->
-	<?php
 	endif;
-	?>
-
-
-	<?php
-	while (have_posts()) :
-		the_post();
-
-		get_template_part('template-parts/content', 'page');
-
-		// If comments are open or we have at least one comment, load up the comment template.
-		if (comments_open() || get_comments_number()) :
-			comments_template();
-		endif;
-
-	endwhile; // End of the loop.
-	?>
-
+	wp_reset_postdata(); ?>
 </main><!-- #main -->
 
 <?php
