987b7e14e4bac43239d3fe7772faaf548df800a4
cody <hi@codywhitby.net>
2021-01-28 10:29:58 -0500

    tax -> posts

--- page.php
+++ page.php
@@ -58,21 +58,23 @@ get_header();
 	endif;
 	wp_reset_postdata();
 
-if( have_rows('home_featured_posts', 'options') ):
-		echo("<div class='home-featured-wrap'>");
-    while( have_rows('home_featured_posts', 'options') ) : the_row();
-				$headline = get_sub_field('headline');
-        $description = get_sub_field('description');
-        // $featured = get_sub_field('post_feature');
-				// echo(print_r($featured, true));
-				echo("<div class='home-featured-cat'>");
-					echo ("<h4>".$headline."</h4>");
-					echo ("<small>".$description."</small>");
-				echo("</div>");
-    endwhile;
-		echo("</div>");
-endif;
-?>
+	if (have_rows('home_featured_posts', 'options')) :
+		echo ("<div class='home-featured-wrap'>");
+		while (have_rows('home_featured_posts', 'options')) : the_row();
+			$headline = get_sub_field('subheadline');
+			$description = get_sub_field('description');
+			$featured = get_sub_field('post_feature');
+			$link = get_permalink($featured->ID);
+			$title = $featured->post_title;
+			echo ("<div class='home-featured-cat'>");
+			echo ("<h5 style='margin:20px 0 0;'><small>" . $headline . "</small></h5>");
+			echo ("<h4 style='margin:5px 0 15px;'><a href='" . $link . "'>" . $title . "</a></h4>");
+			echo ("<small>" . $description . "</small>");
+			echo ("</div>");
+		endwhile;
+		echo ("</div>");
+	endif;
+	?>
 
 
 </main><!-- #main -->
