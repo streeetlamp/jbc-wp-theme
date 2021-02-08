e9da0401d528b454e165f90d5eefeeae0d36a7d8
cody <hi@codywhitby.net>
2021-02-08 15:19:08 -0500

    slight formatting quotes

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.22');
+	define('_S_VERSION', '1.0.23');
 }
 
 if (!function_exists('jbc_setup')) :
