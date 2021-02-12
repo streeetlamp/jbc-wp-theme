4b68ba23bcba4e37f03435acc73dea48af40320e
cody <hi@codywhitby.net>
2021-02-12 10:56:55 -0500

    do we need next/prev

--- single.php
+++ single.php
@@ -18,13 +18,6 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'jbc' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'jbc' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
 		endwhile; // End of the loop.
 		?>
 
