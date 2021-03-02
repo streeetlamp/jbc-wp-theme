9f547cb9cde393728bc7fdbbd93864db35449402
cody <hi@codywhitby.net>
2021-03-02 15:59:03 -0500

    remove more stuff

--- functions.php
+++ functions.php
@@ -260,11 +260,6 @@ function jbc_before_admin_bar_render()
 }
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
@@ -273,11 +268,6 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
