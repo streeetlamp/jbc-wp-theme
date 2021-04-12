74b269abb33fbd9ff1a6f87099343fa7c0174dc2
cody <hi@codywhitby.net>
2021-04-12 17:06:36 -0400

    req $

--- functions.php
+++ functions.php
@@ -205,7 +205,7 @@ function jbc_scripts() {
 
 	wp_enqueue_script( 'jbc-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'jbc-feedback', get_template_directory_uri() . '/js/feedback.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'jbc-feedback', get_template_directory_uri() . '/js/feedback.js', array( 'jquery' ), _S_VERSION, true );
 
 	// if (is_singular() && comments_open() && get_option('thread_comments')) {
 	// wp_enqueue_script('comment-reply');
