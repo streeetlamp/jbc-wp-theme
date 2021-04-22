69da8f24e833683adadf35074f976129c8613ff3
Todd Easter <eeaster@vcu.edu>
2021-04-22 10:08:28 -0400

    navigation style and button refresh

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
 				get_search_form();
 				?>
 				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jbc-wp-theme' ); ?></button>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fas fa-bars"></i> Menu</button>
 					<?php
 					wp_nav_menu(
 						array(
