37a5922a9e88487b9410f513bec462569bb7d5e7
cody <hi@codywhitby.net>
2021-06-11 11:56:02 -0400

    v

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.62' );
+	define( '_S_VERSION', '1.0.65' );
 }
 
 if ( ! function_exists( 'jbc_setup' ) ) :
