c5bcbb27f89d9cd2b97f8925dc2757300e27d50b
cody <hi@codywhitby.net>
2021-06-11 12:28:44 -0400

    v bump

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.65' );
+	define( '_S_VERSION', '1.0.66' );
 }
 
 if ( ! function_exists( 'jbc_setup' ) ) :
