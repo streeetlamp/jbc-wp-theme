09e8a191705caa2bc3093ed79ce85b3122d5e831
cody <hi@codywhitby.net>
2021-07-21 16:37:22 -0400

    sort order

--- functions.php
+++ functions.php
@@ -216,9 +216,9 @@ function jbc_list_child_pages() {
 	global $post;
 
 	if ( is_page() && $post->post_parent ) {
-		$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+		$childpages = wp_list_pages( 'sort_order=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
 	} else {
-		$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+		$childpages = wp_list_pages( 'sort_order=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
 	}
 
 	if ( $childpages ) {
