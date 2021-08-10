9f46e914d6c9e8a543968a1010cee8c5ac8b49ce
cody <hi@codywhitby.net>
2021-08-10 12:10:56 -0400

    contact us / link to lib

--- footer.php
+++ footer.php
@@ -13,14 +13,16 @@
 ?>
 
 <footer id="colophon" class="site-footer">
+	<?php $contact_url = get_permalink( get_page_by_path( 'contact' ) ); ?>
 	<div class="info-footer">
 		<div class="footer-info">
-			<img src="https://apps.library.vcu.edu/assets/public/images/bm_Libraries_RF_hz_4c_rev" alt="VCU Libraries logo">
+			<a href="https://library.vcu.edu"><img src="https://apps.library.vcu.edu/assets/public/images/bm_Libraries_RF_hz_4c_rev" alt="VCU Libraries logo"></a>
 			<ul>
 				<li>901 Park Ave., Box 842033</li>
 				<li>Richmond, VA 23284-2033</li>
 				<li>Toll-free: (844) 352-7399</li>
 				<li>Local: (804) 828-1111</li>
+				<li><a alt="Contact Us" href="<?php echo esc_html($contact_url)?>">Contact Us</a></li>
 			</ul>
 		</div>
 		<nav id="footer-nav" class="footer-navigation">
