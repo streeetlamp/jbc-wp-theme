5de593606ba4522aee64422a9a3e7719ab3afd9f
cody <hi@codywhitby.net>
2021-08-16 12:56:20 -0400

    banner update

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.76' );
+	define( '_S_VERSION', '1.0.77' );
 }
 
 if ( ! function_exists( 'jbc_setup' ) ) :
