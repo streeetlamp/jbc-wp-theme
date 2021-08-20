2faff78bf593b19a9209bc06e8409734118d7d52
Todd Easter <eeaster@vcu.edu>
2021-08-20 17:17:11 -0400

    reset sitemap

--- 404.php
+++ 404.php
@@ -20,7 +20,7 @@ get_header();
 
 		<article id="post-151" class="page type-page status-publish hentry">
 			<div class="page-content">
-				<p><?php _e( 'Sorry, this is awkward. The page you are looking for is not here. Maybe try the search in the header, the <a href="/sitemap_index.xml">sitemap</a>, or <a href="/contact/">contact us</a> if you need more assistance.', 'jbc-wp-theme' ); ?></p>
+				<p><?php _e( 'Sorry, this is awkward. The page you are looking for is not here. Maybe try the search in the header, the <a href="/sitemap/">sitemap</a>, or <a href="/contact/">contact us</a> if you need more assistance.', 'jbc-wp-theme' ); ?></p>
 
 				<?php
 				the_widget( 'WP_Widget_Tag_Cloud' );
