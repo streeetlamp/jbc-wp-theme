7014655272efbaf15ba29a9767fd62b48f60d2b2
cody <hi@codywhitby.net>
2022-01-07 11:22:09 -0500

    update

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.1.16' );
+	define( '_S_VERSION', '1.1.17' );
 }
 
 if ( ! function_exists( 'jbc_setup' ) ) :
